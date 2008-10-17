f84213705666cdbb225ea98cad49d365c1dc8065
Michel Belleville <michel.belleville@gmail.com>
2008-10-17 14:31:16 +0200

    Better map object and test case.

--- tests/map_test.rb
+++ tests/map_test.rb
@@ -2,12 +2,63 @@ require 'lib/map'
 require 'test/unit'
 
 class MapTest < Test::Unit::TestCase
+  # an empty mao should only contain nil values
   def test_empty_map
     map = Map.new
-	(-5..5).each do |x|
-	  (-5..5).each do |y|
-	    assert_nil map[x,y]
-	  end
-	end
+    points do |x, y|
+      assert_nil map[x, y]
+    end
+  end
+  
+  # putting something on a map should return that very thing
+  def test_put_and_return_something
+    map = Map.new
+    points { |x, y, v| assert_equal(map[x, y] = v, v) }
+  end
+
+  # something put on the map should stay wherever it's put
+  def test_keep_things_put
+    map = filled_map
+    points { |x, y, v| assert_equal(map[x, y], v) }
+  end
+  
+  # iterating through a specific zone
+  def test_zone
+    map = filled_map
+    sequence = []
+    (-6..6).each do |x|
+      (-6..6).each do |y| 
+        sequence << [x, y, map[x, y]]
+      end
+    end
+    map.zone(-6, 6, 6, -6) do |v, x, y|
+      sx, sy, sv = sequence.shift
+      assert_equal x, sx
+      assert_equal y, sy
+      assert_equal v, sv
+    end
+  end
+
+  private
+  
+  # creates a set of points
+  def square
+    @square ||= (-5..5).to_a.collect { |i| (-5..5).to_a }
+  end
+
+  # circles through a square set of points
+  def points(&block)
+    square.each_index do |x|
+      square[x].each do |y|
+        yield x, y, "#{x} - #{y}"
+      end
+    end
+  end
+  
+  # fills a new map with a square set of test values
+  def filled_map
+    map = Map.new
+    points { |x, y, v| map[x, y] = v }
+    map
   end
 end
