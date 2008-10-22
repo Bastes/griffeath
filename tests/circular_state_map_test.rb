bb1fba9fbe75434dfd224b649b54ef46ae885d2c
Michel Belleville <michel.belleville@gmail.com>
2008-10-22 08:10:00 +0200

    Empty tests for circular state map.

---
+++ tests/circular_state_map_test.rb
@@ -0,0 +1,10 @@
+require 'lib/state_map'
+require 'test/unit'
+require 'tests/map_test_utilities'
+
+module Griffeath
+  class CircularStateMapTest < Test::Unit::TestCase
+    include MapTestUtilities
+    
+  end
+end
