64a6e4b73deb90daa34e8ee8bf70fc93bf949bc2
Michel Belleville <michel.belleville@gmail.com>
2008-10-26 23:17:20 +0100

    Adding (empty) GriffeathTest.

---
+++ tests/griffeath_test.rb
@@ -0,0 +1,15 @@
+require 'test/unit'
+require 'lib/griffeath'
+
+module Griffeath
+  class CircularStateArrayTest < Test::Unit::TestCase
+    # a new map should have 4 states by default
+    def test_default_states
+    end
+    
+    # the map evolving should follow griffeath's rules
+    def test_evolution
+    end
+  end
+end
+ 
