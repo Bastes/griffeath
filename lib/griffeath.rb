64d07c1ab9f8ce7a7e3ac646d884498c92eb4e5f
Michel Belleville <michel.belleville@gmail.com>
2008-11-10 17:34:22 +0100

    Better tests for evolution, evolution near complete.

--- lib/griffeath.rb
+++ lib/griffeath.rb
@@ -7,25 +7,29 @@ module Griffeath # :nodoc:
     end
 
     def evolution!
-      result = Hash.new
+      result = {}
       @cells.each do |pos, value|
-        around pos, true do |cpos|
-          unless result.has_key? cpos
-            result[cpos] = eval_next cpos
+        x, y = coordinates(pos)
+        around(x, y, true) do |v, x, y|
+          cpos = position(x, y)
+          unless result.has_key? cpos 
+            result[cpos] = eval_next(x, y) 
           end
         end
       end
+      result.delete_if { |k, v| v == states.state }
       @cells = result
+      self
     end
     
-    def eval_next(pos)
-      former_value = self[pos]
-      next_value = next_state(former_value)
+    def eval_next(x, y)
+      former_value = self[x, y]
+      next_value = states.next(former_value)
       value_count = 0
-      values_around pos do |value|
-        value_count += 1 if value == next_value
+      around(x, y) do |v, x, y|
+        value_count += 1 if v == next_value
       end
-      value == 3 ? next_value : former_value
+      value_count >= 3 ? next_value : former_value
     end
   end
 end
